4c5eac266643a2daba7ee77bd61003196aeb9ffd
sadielevy <levy.sad@northeastern.edu>
2023-01-09 10:53:12 -0500

    try browser routing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
-  HashRouter
+  BrowserRouter
 } from "react-router-dom";
 import App from './App';
 import './index.css';
@@ -19,9 +19,9 @@ ReactDOM.createRoot(
 )
 .render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
     <App/>
-    </HashRouter> 
+    </BrowserRouter> 
   </React.StrictMode>,
     document.getElementById("root"),
 
